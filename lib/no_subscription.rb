dde2e7ab6d06e207e06f2b41f2d95a3cf798d944
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-08-13 14:18:44 -0400

    Initial commit

---
+++ lib/no_subscription.rb
@@ -0,0 +1,11 @@
+class NoSubscription
+  def has_mentoring?
+    false
+  end
+  def charge(creditcard)
+    nil
+  end
+  def price
+    0
+  end
+ end
---
+++ lib/user.rb
@@ -0,0 +1,17 @@
+class User
+  include ActiveModel::Model
+  attr_accessor :credit_card, :subscription
+   def charge
+    subscription.charge(credit_card)
+  end
+   def has_mentoring?
+    subscription.has_mentoring?
+  end
+   def price
+    subscription.price
+  end
+   private
+  def subscription
+    @subscription || NoSubscription.new
+  end
+ end
